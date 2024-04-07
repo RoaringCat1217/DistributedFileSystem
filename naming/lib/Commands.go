753c1345be20cb4c299301d8ed294931962a08b2
EthanL <yflin001217@gmail.com>
2024-04-07 18:41:16 -0400

    passed TestFinal_Naming_Replication

--- naming/lib/Commands.go
+++ naming/lib/Commands.go
@@ -62,7 +62,7 @@ func (s *NamingServer) storageDeleteCommand(file *FileInfo, storageServer *Stora
 
 func (s *NamingServer) storageCopyCommand(file *FileInfo, dst *StorageServerInfo, src *StorageServerInfo) bool {
 	url := fmt.Sprintf("http://localhost:%d/storage_copy", dst.commandPort)
-	body := bytes.NewReader([]byte(fmt.Sprintf(`{"path":"%s", "server_ip": "localhost", "server_port": %d}`, file.path, src.clientPort)))
+	body := bytes.NewReader([]byte(fmt.Sprintf(`{"path":"%s", "server_ip": "127.0.0.1", "server_port": %d}`, file.path, src.clientPort)))
 	resp, err := http.Post(url, "application/json", body)
 	if err != nil {
 		fmt.Println(err.Error())
